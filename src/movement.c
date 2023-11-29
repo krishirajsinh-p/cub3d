06568adff40a7e00cd8508c6f6a0bac4cd385c03
kpuwar <kpuwar@student.42heilbronn.de>
2023-11-29 22:25:13 +0100

    movements.c fixed

--- src/movement.c
+++ src/movement.c
@@ -6,38 +6,31 @@
 /*   By: kpuwar <kpuwar@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 01:04:19 by kpuwar            #+#    #+#             */
-/*   Updated: 2023/11/29 03:36:44 by kpuwar           ###   ########.fr       */
+/*   Updated: 2023/11/29 22:22:20 by kpuwar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
-void	move(t_game_data *game_data, t_string *map)
+void	move(t_game_data *game_data, t_string *map, t_vector *vec)
 {
 	double		w_x;
 	double		w_y;
-	t_vector	pos;
 
-	pos = game_data->vectors[POS];
-	w_x = pos.x * MV;
-	w_y = pos.y * MV;
-	// move2(vectors->player_position[Y] + (fact_x), vectors->player_position[X] - (fact_y), map, vectors);
+	w_x = vec[DIR].x * MV;
+	w_y = vec[DIR].y * MV;
 	if (mlx_is_key_down(game_data->mlx, MLX_KEY_D))
-		if (map[(int)(pos.y + w_x)][(int)(pos.x - w_y)] != '1')
-			game_data->vectors[POS] = (t_vector){pos.x - w_y, pos.y + w_x};
-	// move2(vectors->player_position[Y] - (fact_x), vectors->player_position[X] + (fact_y), map, vectors);
+		if (map[(int)(vec[POS].y + w_x)][(int)(vec[POS].x - w_y)] != '1')
+			vec[POS] = (t_vector){vec[POS].x - w_y, vec[POS].y + w_x};
 	if (mlx_is_key_down(game_data->mlx, MLX_KEY_A))
-		if (map[(int)(pos.y - w_x)][(int)(pos.x + w_y)] != '1')
-				game_data->vectors[POS] = (t_vector){pos.x + w_y, pos.y - w_x};
-	// move2(vectors->player_position[Y] + (fact_y), vectors->player_position[X] + (fact_x), map, vectors);
+		if (map[(int)(vec[POS].y - w_x)][(int)(vec[POS].x + w_y)] != '1')
+			vec[POS] = (t_vector){vec[POS].x + w_y, vec[POS].y - w_x};
 	if (mlx_is_key_down(game_data->mlx, MLX_KEY_W))
-		if (map[(int)(pos.y + w_y)][(int)(pos.x + w_x)] != '1')
-				game_data->vectors[POS] = (t_vector){pos.x + w_x, pos.y + w_y};
-	// move2(vectors->player_position[Y] - (fact_y), vectors->player_position[X] - (fact_x), map, vectors);
+		if (map[(int)(vec[POS].y + w_y)][(int)(vec[POS].x + w_x)] != '1')
+			vec[POS] = (t_vector){vec[POS].x + w_x, vec[POS].y + w_y};
 	if (mlx_is_key_down(game_data->mlx, MLX_KEY_S))
-		if (map[(int)(pos.y - w_y)][(int)(pos.x - w_x)] != '1')
-				game_data->vectors[POS] = (t_vector){pos.x - w_x, pos.y - w_y};
-
+		if (map[(int)(vec[POS].y - w_y)][(int)(vec[POS].x - w_x)] != '1')
+			vec[POS] = (t_vector){vec[POS].x - w_x, vec[POS].y - w_y};
 }
 
 void	rotate(t_vector	*vectors, short sign)
